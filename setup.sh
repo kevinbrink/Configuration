f5b3dfc5c665095f2d4bd71e2e512086529bb857
Kevin Brink <Kevin@Kevins-MacBook-Pro.local>
2016-05-30 14:28:49 -0400

    Tweak setup to add git stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+# TODO: Fix these:
 ln -s ./vimrc ~/.vimrc
 ln -s ./vim ~/.vim
 ln -s ./bashrc ~/.bash_profile
 ln -s ./haml-lint.yml ~/.haml-lint.yml
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
