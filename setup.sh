c3c1cae36d154f72935d964960086c642187e35d
Kevin Brink <Kevin@Kevinbrink.com>
2016-06-02 14:51:11 -0400

    Add persistent undo setup

--- setup.sh
+++ setup.sh
@@ -6,5 +6,10 @@ ln -sf $PWD/vim ~/.vim
 ln -sf $PWD/bashrc ~/.bash_profile
 ln -sf $PWD/haml-lint.yml ~/.haml-lint.yml
 
+# Create folder for persistent undo
+mkdir ~/.vim/undo
+# Overkill, but it works:
+chmod 777 ~/.vim/undo
+
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
