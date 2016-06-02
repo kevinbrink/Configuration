4349ac43dc22bac016cb7c1b0a2495a516015253
Kevin Brink <kevin@kevinbrink.com>
2016-06-02 13:48:21 +0000

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # TODO: Fix these:
-ln -s ./vimrc ~/.vimrc
-ln -s ./vim ~/.vim
-ln -s ./bashrc ~/.bash_profile
-ln -s ./haml-lint.yml ~/.haml-lint.yml
+ln -sf $PWD/vimrc ~/.vimrc
+ln -sf $PWD/vim ~/.vim
+ln -sf $PWD/bashrc ~/.bash_profile
+ln -sf $PWD/haml-lint.yml ~/.haml-lint.yml
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
